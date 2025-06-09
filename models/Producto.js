99ecdb5b74c483eb042a01d63e21727a2801202f
Matiasgma <matiasgma95@gmail.com>
2025-06-09 16:09:08 -0400

    Corregir nombre de schema: guitarraSchema → productoSchema

--- models/Producto.js
+++ models/Producto.js
@@ -1,18 +1,25 @@
-const mongoose = require('mongoose')
-const productoSchema = mongoose.Schema({
-        nombre: {
-            type: String, 
-            required: true
-            ,
-  usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' } },
-        precio: {
-            type: Number
-        },
-    },
-    {
-        timestamps: true
-    }
-)
-const Producto = mongoose.model('Producto', guitarraSchema)
+const mongoose = require('mongoose');
 
-module.exports = Guitarra
+const productoSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    precio: {
+      type: Number,
+      required: true,
+    },
+    usuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Usuario',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Producto = mongoose.model('Producto', productoSchema);
+module.exports = Producto;
