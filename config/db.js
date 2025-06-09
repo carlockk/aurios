04acd162f19f79b7f379837a9a70a3affeb2eb29
Matiasgma <matiasgma95@gmail.com>
2025-06-09 17:38:43 -0400

    actualizacion de archivos, se forzo a db a conectarse directo a la base de datos porque me creaba un test

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: process.env.DB_NAME || 'aurios',
+        })
         console.log("connected to the database")
     } catch (error) {
         console.log(error)
