62c0853785e18d0a4579cf391aa8f4c724dd2eca
carlockk <carlos.virtualdesk@gmail.com>
2025-06-10 00:30:39 -0400

    userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,10 +7,10 @@ exports.createUser = async (req, res) => {
 	// obtener usuario, email y password de la petición
 	const { username, email, password } = req.body    
 	try {
-		// Generemos un fragmento aleatorio para usarse con el password
+		// fragmento aleatorio para usarse con el password
 		const salt = await bcryptjs.genSalt(10)
 		const hashedPassword = await bcryptjs.hash(password, salt)
-		// creamos un usuario con su password encriptado
+		// crea un usuario con su password encriptado
 		const respuestaDB = await Usuario.create({
 			username, 
 			email, 
@@ -27,30 +27,30 @@ exports.createUser = async (req, res) => {
 
 exports.login = async(req, res) => {
 
-    // obtenemos el email y password de la petición
+    // obtiene el email y password de la petición
     const {email, password} = req.body
     try {
-        // buscamos al usuario
+        // busca al usuario
         let foundUser = await Usuario.findOne({email})
-        // si no se encuentra al usuario, devolvemos un error
+        // si no se encuentra al usuario,da un error
         if(!foundUser){
             return res.status(400).json({msg: "Username does not exist"})
         }
-        // si lo encuentra, evaluamos si la contraseña es correcta
+        // si lo encuentra, evalua si la contraseña es correcta
         const passCorrecto = await bcryptjs.compare(password, foundUser.password)
-        // si la contraseña es incorrecta, lo reportamos 
-        // debemos tener cuidado de no entregar más info de la estrictamente necesaria
+        // si la contraseña es incorrecta, lo reporta 
+        // con cuidado de no entregar más info de la estrictamente necesaria
         if(!passCorrecto){
             return await res.status(400).
 	            json({msg: "The username or password does not correspond"})
         }
-        // si todo es correcto, generamos un json web token
+        // si todo es correcto, generam un json web token
         // 1. el 'payload' será un objeto que contendrá el id del usuario
         const payload = { user: { id: foundUser.id } }
         // 2. firma del jwt
         jwt.sign(
             payload, 
-            // usamos la palabra secreta para descifrar la firma electrónica del token
+            // usa la palabra secreta para descifrar la firma electrónica del token
             process.env.SECRET,
             {
                 expiresIn: 3600 // expiración del token
@@ -70,12 +70,12 @@ exports.login = async(req, res) => {
 
 exports.verifyToken = async (req, res) => {
 	try {
-		// confirmamos que el usuario exista y 
+		// confirma que el usuario exista y 
 		//retorna sus datos, excluyendo de password
 		const usuario = await Usuario.findById(req.user.id).select('-password')
 		res.json({ usuario })
 	} catch (error) {
-		// en caso de error, devolvemos un mensaje
+		// en caso de error, devolver un mensaje
 		res.status(500).json({
 			msg: "we have an error",
 			error
