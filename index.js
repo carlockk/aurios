15d3719a8142b29892733f415ff2510d2c6ba2c1
Matiasgma <matiasgma95@gmail.com>
2025-06-09 23:19:53 -0400

    index actualizado

--- index.js
+++ index.js
@@ -11,17 +11,21 @@ const productRouter = require('./routes/productRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 
+// Conexión a base de datos
 connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// ✅ Swagger debe estar antes de las rutas
+// Documentación Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// Rutas API
 app.use('/api/users', userRouter);
 app.use('/api/product', productRouter);
 
-app.listen(process.env.PORT, () =>
-  console.log('Servidor escuchando en el puerto ' + process.env.PORT)
-);
+// Servidor
+app.listen(process.env.PORT, () => {
+  console.log('Servidor escuchando en el puerto ' + process.env.PORT);
+});
